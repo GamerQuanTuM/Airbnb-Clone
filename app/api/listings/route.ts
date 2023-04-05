bc2560a25a4b719028ff9d4f927fd2e39826216f
Shuvam <shuvamsantra10@gmail.com>
2023-04-05 18:59:21 +0530

    Completed Rent Modal

---
+++ app/api/listings/route.ts
@@ -0,0 +1,48 @@
+import { NextResponse } from "next/server";
+import prisma from "@/libs/prismadb";
+import User from "@/actions/getCurrentUser";
+
+
+export async function POST(request: Request) {
+    const user = await User();
+
+    if (!user) {
+        return NextResponse.error()
+    }
+
+    const body = await request.json()
+
+    const {
+        category,
+        location,
+        guestCount,
+        bathroomCount,
+        roomCount,
+        imageSrc,
+        price,
+        title,
+        description
+    } = body
+
+    Object.keys(body).forEach((value: any) => {
+        if (body[value]) {
+            return NextResponse.error()
+        }
+    });
+
+    const listing = await prisma.listing.create({
+        data: {
+            category,
+            locationValue: location.value,
+            guestCount,
+            bathroomCount,
+            roomCount,
+            imageSrc,
+            price: parseInt(price, 10),
+            title,
+            description,
+            userId: user.id
+        }
+    })
+    return NextResponse.json(listing, { status: 201 })
+}
